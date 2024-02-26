875966ef3402801ea86b6830543ab164902dfcfc
kishor-karthik-dev <18mech094@gmail.com>
2024-02-27 00:00:03 +0530

    okk

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+// import { phone } from 'phone';
+import { FaRegCircleUser } from "react-icons/fa6";
+import { MdOutlineEmail } from "react-icons/md";
+import { FaPhoneAlt } from "react-icons/fa";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { BiShow } from "react-icons/bi";
+import React from 'react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='continer'>
+        <form>
+          <div className='title'>
+            <h1>Sign-Up</h1>
+          </div>
+          <div className='inputs'>
+          <FaRegCircleUser className='icon' />
+            <input type='text' placeholder='Name' required></input>
+          </div>
+          <div className='inputs'>
+          <MdOutlineEmail className='icon' />
+            <input type='email' placeholder='Email' required></input>
+          </div>
+          <div className='inputs'>
+          <FaPhoneAlt className='icon' />
+            <input type='phoneNumber' placeholder='Number' required></input>
+          </div>
+          <div className='inputs'>
+          <BiShow onClick={() => { setShow(!show) }}className='icon' />
+          <RiLockPasswordFill className='icon' />
+            <input type='password' placeholder='password' required></input>
+          </div>
+
+          <div className='btn'>
+            <button >SignUp</button>
+          </div>
+          <div className='for'>
+          <a href='forgetpassword'> forgotpassword?</a>
+          <a href='create account'>create account </a>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
